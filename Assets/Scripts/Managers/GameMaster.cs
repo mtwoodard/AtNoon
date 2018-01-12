2d6d8bfed33b2710673dd3f6035ea9d17507972e
Michael Woodard <1995mtwoodard@gmail.com>
2018-01-11 20:09:32 -0600

    Basic Unity Initial Files + Others
    Also includes Game Master UserSettings and  a couple of folders to be
    used.

---
+++ Assets/Scripts/Managers/GameMaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+    private static GameMaster _instance;
+    
+    public static GameMaster Instance { get { return _instance; } }
+    public UserSettings userSettings;
+
+    private void Awake()
+    {
+        //Singleton -------------------------------
+        if (_instance != null && _instance != this)
+            Destroy(this.gameObject);
+        else
+            _instance = this;
+        // ----------------------------------------
+        // Init UserSettings ----------------------
+        userSettings = UserSettings.Load();
+        // ----------------------------------------
+    }
+}
---
+++ Assets/Scripts/Utility/UserSettings.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class UserSettings : MonoBehaviour {
+
+    public int dayProgress;        //What day in the game are we on
+    public float volumeSFX;     
+    public float volumeVoice;
+    public float volumeBG;
+    public float volumeMusic;
+    public KeyCode actionKey;
+    public KeyCode jumpKey;
+    public KeyCode watchKey;
+
+    public UserSettings()
+    {
+        dayProgress = 0;
+        actionKey = KeyCode.E;
+        jumpKey = KeyCode.Space;
+        watchKey = KeyCode.T;
+        volumeSFX = 100f;
+        volumeVoice = 100f;
+        volumeBG = 100f;
+        volumeMusic = 100f;
+}
+
+public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/userSettings.dat", FileMode.Open);
+        bf.Serialize(file, this);
+        file.Close();
+    }
+
+    public static UserSettings Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/userSettings.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/userSettings.dat", FileMode.Open);
+            UserSettings us =  (UserSettings)bf.Deserialize(file);
+            file.Close();
+            return us;
+        }
+        else // Usually due to it being the first time the game has been opened. If not, uh oh
+        {
+            Debug.Log("No previous data found");
+            UserSettings us = new UserSettings();
+            return us;
+        }
+        
+    }
+
+    public void ClearSave() // Only clear stuff like progress and unlocks
+    {
+        UserSettings us = new UserSettings();
+        us.actionKey = GameMaster.Instance.userSettings.actionKey;
+        us.jumpKey = GameMaster.Instance.userSettings.jumpKey;
+        us.watchKey = GameMaster.Instance.userSettings.watchKey;
+        us.volumeSFX = GameMaster.Instance.userSettings.volumeSFX;
+        us.volumeVoice = GameMaster.Instance.userSettings.volumeVoice;
+        us.volumeBG = GameMaster.Instance.userSettings.volumeBG;
+        us.volumeMusic = GameMaster.Instance.userSettings.volumeMusic;
+        us.Save();
+    }
+}
