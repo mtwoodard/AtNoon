fda631657043fc2bc4c59fcc0f8b6ffc41359907
Michael Woodard <1995mtwoodard@gmail.com>
2018-02-21 16:46:19 -0600

    Added ProBuilder/ProGrids asset packages as well as a character controller, smooth mouse look, and globals toolbox

---
+++ Assets/Scripts/Controllers/Character/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Controllers/Player Controller")]
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 7.0f;
+    public float jumpStrength = 5.0f;
+    public float terminalVelocity = 52.0f;
+    public bool movementLock = false;
+
+    Vector3 movement = Vector3.zero;
+    Vector3 gravity = Vector3.zero;
+    CharacterController player;
+
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (!movementLock)
+        {
+
+            movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * speed;
+            movement = transform.TransformDirection(movement);
+            if (!player.isGrounded)
+            {
+                gravity += Physics.gravity * Time.deltaTime;
+                gravity.y = Mathf.Max(gravity.y, -terminalVelocity);
+            }
+            else
+            {
+                gravity = Vector3.zero;
+                if (jump)
+                    gravity.y = jumpStrength;
+            }
+
+            movement += gravity;
+            player.Move(movement * Time.deltaTime);
+        }
+        else
+        {
+            movement = Vector3.zero;
+            if (!player.isGrounded)
+            {
+                gravity += Physics.gravity * Time.deltaTime;
+                gravity.y = Mathf.Max(gravity.y, -terminalVelocity);
+            }
+            else
+            {
+                gravity = Vector3.zero;
+                if (jump)
+                    gravity.y = jumpStrength;
+            }
+            movement += gravity;
+            player.Move(movement * Time.deltaTime);
+        }
+    }
+
+
+
+    bool jump
+    {
+        get
+        {
+            return Input.GetKeyDown(KeyCode.Space);
+        }
+    }
+
+    public bool isGrounded
+    {
+        get
+        {
+            return player.isGrounded;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/Globals.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals : MonoBehaviour {
+    private static Globals _instance;
+    public static Globals Instance { get { return _instance; } }
+
+    private Dictionary<string, GameObject> dictionary;
+
+    private void Awake()
+    {
+        //Singleton -------------------------------
+        if (_instance != null && _instance != this)
+            Destroy(this.gameObject);
+        else
+            _instance = this;
+        // ----------------------------------------
+        dictionary = new Dictionary<string, GameObject>();
+    }
+
+    public void RegisterGlobal(string nameOfObject, GameObject item)
+    {
+        dictionary.Add(nameOfObject, item);
+    }
+    
+    public GameObject Global(string nameOfObject)
+    {
+        GameObject item;
+        if (dictionary.TryGetValue(nameOfObject, out item))
+            return item;
+        else
+        {
+            item = new GameObject();
+            item.name = "not found";
+            return item;
+        }
+
+    }
+
+    public void DeleteGlobal(string nameOfObject)
+    {
+        dictionary.Remove(nameOfObject);
+    }
+}
