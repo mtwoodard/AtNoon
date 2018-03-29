5caa1f47591388e15db459d153244f265e8c0cde
Michael Woodard <1995mtwoodard@gmail.com>
2018-03-29 16:32:11 -0500

    Added a menun scene as well as the levelloader created prefabs folder

---
+++ Assets/Scripts/Utility/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+    public GameObject loadScreen;
+    public Slider slider;
+
+    public void LoadLevel(string sceneName)
+    {
+        StartCoroutine(LoadAsync(sceneName));
+    }
+
+    IEnumerator LoadAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        loadScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            Debug.Log(progress);
+            slider.value = progress;
+            yield return null;
+        }
+    }
+}
