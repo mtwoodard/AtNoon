08ea821312af55aed1f0dd12f285de53c269aec9
Brayden Dyke <brayden.dyke@gmail.com>
2018-04-05 17:48:34 -0500

    Firing and Damage Mechanics

---
+++ Assets/Scripts/Pistol/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float health = 50f;
+
+	public void TakeDamage(float damage) {
+		health -= damage;
+
+		if (health <= 0f) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Pistol/Gun.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	public float damage = 10f;
+	public float range = 100f;
+	public Camera cam;
+	public ParticleSystem muzzleFlash;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonDown("Fire1")) {
+			Shoot();
+		}
+	}
+	void Shoot () {
+		//muzzleFlash.Play ();
+
+		RaycastHit hit;
+		if (Physics.Raycast (cam.transform.position, cam.transform.forward, out hit, range)) {
+			Debug.Log (hit.transform.name);
+
+			Enemy enemy = hit.transform.GetComponent<Enemy> ();
+			if (enemy != null) {
+				enemy.TakeDamage (damage);
+			}
+		}
+	}
+}
