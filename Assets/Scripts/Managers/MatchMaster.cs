03a4444781938432bd66b892240f321851871988
mtwoodard <1995mtwoodard@gmail.com>
2018-04-19 18:31:01 -0500

    Added state control to game master and worked on functionality of the match master

--- Assets/Scripts/Managers/MatchMaster.cs
+++ Assets/Scripts/Managers/MatchMaster.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 
 public class MatchMaster : MonoBehaviour {
     enum MatchStates { Start, Play, GameOver, LostConnection}
+    struct PlayerMatchStats
+    {
+        public int kills, deaths;
+        public string name, team;
+    }
 
     public Text timeLeft;
     public Text draw;
+
     MatchStates matchState;
+    ArrayList matchStats = new ArrayList();
     Timer timer;
     SmoothMouseLook mainCamera;
     // Use this for initialization
     void Start () {
         matchState = MatchStates.Start;
         timer = gameObject.AddComponent<Timer>();
-        timeLeft.gameObject.SetActive(false);
-        StartCoroutine(StartMatch());
         mainCamera = Globals.Instance.Global("MainCamera").GetComponent<SmoothMouseLook>();
         mainCamera.cameraLock = true;
 	}
@@ -25,9 +30,13 @@ public class MatchMaster : MonoBehaviour {
     {
         switch (matchState)
         {
-            case MatchStates.Start: //
+            case MatchStates.Start: // Spawn players in and initialize their data
+                //Init Players Code Here
+                StartCoroutine(StartMatch());
+                matchState = MatchStates.Play;
                 break;
-            case MatchStates.Play: //Run timer update match stats
+            case MatchStates.Play: //Run timer update match stats update gui
+                timeLeft.text = "" + (int)timer.timeLeft;
                 break;
             case MatchStates.GameOver: //Show ending score card
                 break;
@@ -36,11 +45,11 @@ public class MatchMaster : MonoBehaviour {
                 break;
 
         }
-        timeLeft.text = "" + (int)timer.timeLeft;
     }
 	
     private IEnumerator StartMatch()
     {
+        timeLeft.gameObject.SetActive(false);
         yield return new WaitForSeconds(Random.Range(2f, 6f));
         draw.gameObject.SetActive(true);
         yield return new WaitForSeconds(1f);
